61dda3962340e29bb6d5b76b60f5aa704088d1ca
Brice Rauby <brice.rauby@polymtl.ca>
2023-02-01 15:36:04 -0500

    fast training and resnet fast code

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # IFT6164-FourierAnalysisAdversarialExamples
+
+## Acknowledgment
+- For the pgd attack computation : https://github.com/Harry24k/PGD-pytorch/blob/master/PGD.ipynb 
+- For the fast training resnet : https://www.kaggle.com/code/kmldas/cifar10-resnet-90-accuracy-less-than-5-min 
+- For the training script for resnet : https://github.com/ndb796/Pytorch-Adversarial-Training-CIFAR
---
+++ train_resnet_fast.py
@@ -0,0 +1,154 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+import tqdm
+import torch.optim as optim
+import datetime
+
+
+# https://www.kaggle.com/code/kmldas/cifar10-resnet-90-accuracy-less-than-5-min
+
+seed=1
+torch.manual_seed(seed)
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+
+epochs = 50
+max_lr = 0.01
+grad_clip = 0.1
+final_div_factor = 1e4
+weight_decay = 1e-4
+opt_func = torch.optim.Adam
+modelType = "resnet18"
+batch_size = 400
+
+def accuracy(outputs, labels):
+    _, preds = torch.max(outputs, dim=1)
+    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
+
+
+
+
+@torch.no_grad()
+def evaluate(model, val_loader, device):
+    model.eval()
+    outputs = [validation_step(model, batch, device) for batch in val_loader]
+    return validation_epoch_end(outputs)
+
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group['lr']
+
+def training_step(model, batch, device):
+    images, labels = batch
+    images = images.to(device)
+    labels = labels.to(device)
+    out = model(images)                    # Generate predictions
+    loss = F.cross_entropy(out,labels)
+    return loss
+
+def validation_step(model, batch, device):
+    images, labels = batch
+    images = images.to(device)
+    labels = labels.to(device)
+    out = model(images)                    # Generate predictions
+    loss = F.cross_entropy(out,labels)   # Calculate loss
+    acc = accuracy(out, labels)           # Calculate accuracy
+    return {'val_loss': loss.detach(), 'val_acc': acc}
+
+def validation_epoch_end(outputs):
+    batch_losses = [x['val_loss'] for x in outputs]
+    epoch_loss = torch.stack(batch_losses).mean()   # Combine losses
+    batch_accs = [x['val_acc'] for x in outputs]
+    epoch_acc = torch.stack(batch_accs).mean()      # Combine accuracies
+    return {'val_loss': epoch_loss.detach(), 'val_acc': epoch_acc}
+
+def epoch_end(epoch, result):
+    print("Epoch [{}], last_lr: {:.5f}, train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
+        epoch, result['lrs'][-1], result['train_loss'], result['val_loss'], result['val_acc']))
+
+        
+
+def fit_one_cycle(epochs, max_lr, model, train_loader, val_loader, 
+                  weight_decay=0, grad_clip=None, opt_func=torch.optim.SGD, device='cpu'):
+    torch.cuda.empty_cache()
+    history = []
+    
+    # Set up cutom optimizer with weight decay
+    optimizer = opt_func(model.parameters(), max_lr, weight_decay=weight_decay)
+    # Set up one-cycle learning rate scheduler
+    sched = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr, epochs=epochs,final_div_factor=final_div_factor, 
+                                                steps_per_epoch=len(train_loader))
+    
+    for epoch in range(epochs):
+        # Training Phase 
+        model.train()
+        train_losses = []
+        lrs = []
+        for batch in train_loader:
+            loss = training_step(model, batch, device)
+            train_losses.append(loss)
+            loss.backward()
+            
+            # Gradient clipping
+            if grad_clip: 
+                nn.utils.clip_grad_value_(model.parameters(), grad_clip)
+            
+            optimizer.step()
+            optimizer.zero_grad()
+            
+            # Record & update learning rate
+            lrs.append(get_lr(optimizer))
+            sched.step()
+        
+        # Validation phase
+        result = evaluate(model, val_loader, device)
+        result['train_loss'] = torch.stack(train_losses).mean().item()
+        result['lrs'] = lrs
+        epoch_end(epoch, result)
+        history.append(result)
+    return history
+
+
+
+
+train_transform = transforms.Compose([transforms.RandomCrop(32, padding=4, padding_mode='reflect'), 
+                         transforms.RandomHorizontalFlip(),
+                        transforms.ToTensor()])
+
+test_transform = transforms.Compose(
+    [transforms.ToTensor()])
+
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=train_transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                          shuffle=True, num_workers=6)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=test_transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                         shuffle=False, num_workers=6)
+
+model = getattr(torchvision.models, modelType)(num_classes=10)
+model.to(device)
+history = fit_one_cycle(epochs, max_lr, model, trainloader, testloader, 
+                             grad_clip=grad_clip, 
+                             weight_decay=weight_decay, 
+                             opt_func=opt_func, device=device)
+
+
+for hist in history : 
+    print('train loss {}, val loss {}, val acc {}'.format(hist['train_loss'],hist['val_loss'],hist['val_acc'] ))
+
+
+time = datetime.datetime.today()
+PATH = './cifar_{}_{}_{}h{}min_seed_{}.pth'.format(modelType, time.date(), time.hour, time.minute,seed)
+torch.save(model.state_dict(), PATH)
